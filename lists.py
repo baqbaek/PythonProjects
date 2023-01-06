b322ffe78901151cf9b51262f1fe2f2f2521a58b
Bartek <baqbaek@gmail.com>
2023-01-06 20:25:16 +0100

    simple list sorting and adding

---
+++ lists.py
@@ -0,0 +1,11 @@
+matrix = [1,2,3,4,5,6,0,4,5,6,10]
+matrix.sort(reverse= True)
+print(matrix[:])
+
+
+
+friends = ['Simon', 'Patty', 'Joy', 'Carrie', 'Amira', 'Chu']
+
+friends.append('Stanley')
+friends.sort()
+print(friends)
