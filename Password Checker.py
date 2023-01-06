76f23f98e62ed6c168ec548ebe3ae5ade759baea
Bartek <baqbaek@gmail.com>
2023-01-06 18:22:00 +0100

    Password checker

--- Password Checker.py
+++ Password Checker.py
@@ -1,6 +1,14 @@
-password = input('What is your password?')
+def check_password(password):
+    if len(password) < 8:
+        return 'Password must be at least 8 characters long'
+    elif not any(char.isdigit() for char in password):
+        return 'Password must contain at least one digit'
+    elif not any(char.isupper() for char in password):
+        return 'Password must contain at least one uppercase letter'
+    elif not any(char.islower() for char in password):
+        return 'Password must contain at least one lowercase letter'
+    else:
+        return 'Password is ok'
 
-print(password)
-
-for i in range (len(password)):
-    print('*', end= '')
+password = input('Enter your password: ')
+print(check_password(password))
