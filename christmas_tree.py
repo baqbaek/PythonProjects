3968eede5e9b583712cfecc8c4f850e839182473
Bartek <baqbaek@gmail.com>
2023-01-07 20:31:03 +0100

    Christmas tree

--- christmas_tree.py
+++ christmas_tree.py
@@ -1,14 +1,14 @@
-#Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
+# Display the image below to the right hand side where the 0 is going to be ' ', and the 1 is going to be '*'. This will reveal an image!
 picture = [
-  [0,0,0,1,0,0,0],
-  [0,0,1,1,1,0,0],
-  [0,1,1,1,1,1,0],
-  [1,1,1,1,1,1,1],
-  [0,0,0,1,0,0,0],
-  [0,0,0,1,0,0,0]
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0]
 ]
-for i in picture:
-    for j in i:
+for i in picture:       #iterrate by rows
+    for j in i:         #iterate by values
         if j == 0:
             print(' ', end='')
         else:
